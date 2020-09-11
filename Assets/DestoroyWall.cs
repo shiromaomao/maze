77fd896c9eddbfb942dea079ae2f012008261a48
shiromaomao <69060224+shiromaomao@users.noreply.github.com>
2020-09-11 19:18:02 +0900

    DestoroyWall
    球を消す壁、坂の壁？、クローンをちょびっと｛
    噴水みたいになった(*ノv*)b[ちょっと面白かった]｝

---
+++ Assets/DestoroyWall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoroyWall : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Sphere"))// もしもぶつかってきたオブジェクトのタグに(Sphere)という名前がついていたら
+        {
+            Destroy(other.gameObject);// ぶつかってきたオブジェクトを破壊（削除）する
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -9,13 +9,19 @@ public class Sphere : MonoBehaviour
     void Start()
     {
         rb1 = GetComponent<Rigidbody>();
-        rb1.AddForce(10,0,0,ForceMode.Impulse);
+        rb1.AddForce(10,0,0,ForceMode.Impulse);//speed
+
+        GameObject original = GameObject.Find("Sphere");
+		GameObject copied = Object.Instantiate(original) as GameObject;
+		copied.transform.Translate(-25,30,-8);
     }
+
+    //public static Object Instantiate(Object original, Vector3 position, Quaterinon rotation);
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < -12)
+        if(transform.position.y < -12)//hukki
         {
             transform.position = new Vector3(-25,29,-7);
         }

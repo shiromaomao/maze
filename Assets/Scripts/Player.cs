161e7e5bd61ae43dbdbea6b197cb13f0c19ba631
shiromaomao <69060224+shiromaomao@users.noreply.github.com>
2020-11-27 19:26:03 +0900

    bug
    Where is program erar?
    バグはどこに？再起動したら、消えました

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,13 +10,12 @@ public class Player : MonoBehaviour
 
     public bool pushedSwich;
 
+    int Keyget = 0;
     void Start()
     {
         pushedSwich = false;
         scoreText.text = "Key: 0"; //初期スコアを代入して表示
     }
-    int Keyget = 0;
-
     // Update is called once per frame
 
     void Update()
@@ -41,13 +40,14 @@ public class Player : MonoBehaviour
         {
             transform.position -= transform.forward * 0.1f;
         }
-        
-        if (transform.position.y < -60) 
+
+        if (transform.position.y < -60)
         {
             transform.position = new Vector3(8.3f, 2, 8.5f);
             float y = -90;
             this.transform.rotation = Quaternion.Euler(0.0f, y, 0.0f);
         }
+
         if (Input.GetKey("s"))
         {
             transform.position = new Vector3(8.3f, 2, 8.5f);
@@ -56,14 +56,21 @@ public class Player : MonoBehaviour
         }
 
         scoreText.text = "Key:" + Keyget;
-
     }
 
     private void OnCollisionEnter(Collision col)
     {
+        Debug.Log(col.gameObject.name); // ぶつかった相手の名前を取得
+        //    if (col.gameObject.tag == "Sphere")
+        if (col.gameObject.tag == "Sphere" || col.gameObject.tag == "Enemy")
+        {
+            transform.position = new Vector3(8.3f, 2, 8.5f);
+            float y = -90;
+            this.transform.rotation = Quaternion.Euler(0.0f, y, 0.0f);
+        }
         //Door
         if (col.gameObject.tag == "Door")
-        {
+        {       
             if (Keyget >= col.gameObject.GetComponent<Door>().openkeynum)
             {
                 Keyget -= col.gameObject.GetComponent<Door>().openkeynum;
@@ -72,33 +79,37 @@ public class Player : MonoBehaviour
         }
 
         //Ereveter
-        if (col.gameObject.tag == "Elevator")
+        /*if (col.gameObject.tag == "Elevator")
         {
-            /*if (Keyget >= col.gameObject.GetComponent<Door>().openkeynum)
+            if (Keyget == col.gameObject.GetComponent<Door>().openkeynum)
             {
                 Keyget -= col.gameObject.GetComponent<Door>().openkeynum;
                 col.gameObject.GetComponent<Door>().Open();
-            }*/
+            }else(Keyget > col.gameObject.GetComponent<Door>().openkeynum);//異常発生　　　要注意
+            {
+                Keyget = 0;
+                col.gameObject.GetComponent<Door>().Open();
+            }
 
             //「=」は、1つで代入。2つで等価。
             this.gameObject.transform.parent = col.gameObject.transform;
-        }
+        }*/
 
         if (col.gameObject.tag == "Switch" && !pushedSwich) //&&　で、「かつ」。　
         //!は、「bool」型の値を反転（否定）　例）！＝で、「異なるなら」となる。
         {
             pushedSwich = true;
             StartCoroutine("stop3");
-        }
+        }    
     }
-    private IEnumerator stop3() //コルーチン関数の名前
-                                //  StartCoroutine("stop3");  の中身
+
+    private IEnumerator stop3() //コルーチン関数の名前  StartCoroutine("stop3");  の中身
     {
-        //コルーチンの内容
         yield return new WaitForSeconds(15.0f);//5秒待つ
         pushedSwich = false;
     }
-    private void OnCollisionExit(Collision col)
+
+    void OnCollisionExit(Collision col)
     {
         if (col.gameObject.tag == "Elevator")
         {
@@ -118,8 +129,5 @@ public class Player : MonoBehaviour
             Destroy(col.gameObject);
             ++Keyget;
         }
-
-
     }
-
 }
